4d9138f369ab0336b678a68b56b03d2efd8104ae
Edward Ocampo-Gooding <edward@edwardog.net>
2009-01-19 15:32:11 -0500

    Minor controller/view cleanup in HomeController#welcome

--- generators/shopify_app/templates/app/controllers/home_controller.rb
+++ generators/shopify_app/templates/app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   
   around_filter :shopify_session, :except => 'welcome'
   
+  def welcome
+    current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
+    @callback_url = "http://#{current_host}/login/finalize"
+  end
+  
   def index
     # get 3 products
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
