aa4b11c4231d693c61bd5a661d67993c5de8b03e
James MacAulay <james@jadedpixel.com>
2009-05-07 11:33:49 -0700

    run shopify.yml through ERB

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'shopify_api'
 require 'shopify_login_protection'
 
 begin
-  ShopifyAPI::Session.setup(YAML.load_file("#{RAILS_ROOT}/config/shopify.yml")[RAILS_ENV])
+  ShopifyAPI::Session.setup(YAML.load(ERB.new(File.read("#{RAILS_ROOT}/config/shopify.yml")).result)[RAILS_ENV])
 
   ActionController::Base.send :include, ShopifyLoginProtection
   ActionController::Base.send :helper_method, :current_shop
