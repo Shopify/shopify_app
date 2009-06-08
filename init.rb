8810bee982d326b8d8f27bcf75bdbd73b6d047d8
Cody Fauser <codyfauser@gmail.com>
2009-06-08 12:23:44 -0400

    Don't require shopify.yml when using environment variables

--- init.rb
+++ init.rb
@@ -1,11 +1,22 @@
 require 'shopify_api'
 require 'shopify_login_protection'
 
-begin
-  ShopifyAPI::Session.setup(YAML.load(ERB.new(File.read("#{RAILS_ROOT}/config/shopify.yml")).result)[RAILS_ENV])
-
-  ActionController::Base.send :include, ShopifyLoginProtection
-  ActionController::Base.send :helper_method, :current_shop
-rescue Errno::ENOENT
-  STDERR.puts '** [!] Shopify app plugin installed but no config/shopify.yml found.'
+if ENV['SHOPIFY_API_KEY'] && ENV['SHOPIFY_API_SECRET']
+  Rails.logger.info "[Shopify App] Loading API credentials from environment"
+  
+  ShopifyAPI::Session.setup(:api_key => ENV['SHOPIFY_API_KEY'], :secret => ENV['SHOPIFY_API_SECRET'])
+else
+  config = File.join(Rails.root, "config/shopify.yml")
+  
+  if File.exist?(config)
+    Rails.logger.info "[Shopify App] Loading API credentials from config/shopify.yml"
+    
+    credentials = YAML.load(File.read(config))[Rails.env]
+    ShopifyAPI::Session.setup(credentials)
+  else
+    Rails.logger.warn '[Shopify App] Shopify App plugin installed but no config/shopify.yml found.'
+  end
 end
+
+ActionController::Base.send :include, ShopifyLoginProtection
+ActionController::Base.send :helper_method, :current_shop
