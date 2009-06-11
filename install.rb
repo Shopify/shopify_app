ac548f9563b148b8ba845e7b71e2ebf302d03cb8
Tobias Lütke <tobi@jadedpixel.com>
2009-06-11 14:33:18 -0400

    Updated some instructions w

--- install.rb
+++ install.rb
@@ -1,3 +1,10 @@
-puts "---------------------------------------------------------------------------"
-puts " Please run ./script/generate shopify_app"
-puts "---------------------------------------------------------------------------"
+puts
+puts "Shopify App generator"
+puts "---------------------"
+puts
+puts "Before you set can get started, you need to register your application with the Shopify Partner UI."
+puts
+puts " * Go to http://www.shopify.com/partners and create or login your partner account. Before you get started with your Shopify app you need to "
+puts " * Register your application on the Application tab. Use http://localhost:3000/login/finalize as redirect URL during development." 
+puts " * run ./script/generate shopify_app <api_key> <secret>"
+puts
