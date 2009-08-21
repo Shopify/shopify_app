571fb639f57be02f5bea401985eff3cc43a0159d
Edward Ocampo-Gooding <edward@edwardog.net>
2009-08-21 14:40:16 -0400

    Clarified installation message

--- install.rb
+++ install.rb
@@ -1,10 +1,11 @@
 puts
-puts "Shopify App generator"
+puts "Shopify App Generator"
 puts "---------------------"
 puts
-puts "Before you set can get started, you need to register your application with the Shopify Partner UI."
+puts "To get started, first register your app as a Shopify Partner:"
 puts
-puts " * Go to http://www.shopify.com/partners and create or login your partner account. Before you get started with your Shopify app you need to "
-puts " * Register your application on the Application tab. Use http://localhost:3000/login/finalize as redirect URL during development." 
-puts " * run ./script/generate shopify_app <api_key> <secret>"
+puts " * Go to http://www.shopify.com/partners and create or login to your Partner account."
+puts " * Jump over to the Apps tab and create your application"
+puts "   (Make sure to set the Return URL to http://localhost:3000/login/finalize during development)"
+puts " * Run ./script/generate shopify_app <api_key> <secret>"
 puts
