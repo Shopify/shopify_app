be608f99e965910b53ea80bdd25c31e7a7e4a9ca
Edward Ocampo-Gooding <edward@edwardog.net>
2011-01-22 23:18:58 -0500

    Updated install message for Rails 3

--- install.rb
+++ install.rb
@@ -5,11 +5,21 @@ puts
 puts "To get started, first register your app as a Shopify Partner:"
 puts
 puts " * Go to http://www.shopify.com/partners and create or login to your Partner account."
+puts
 puts " * Jump over to the Apps tab and hit the 'Create a new app' button"
 puts "   (Make sure to set the Return URL to http://localhost:3000/login/finalize during development)"
-puts " * Install the Shopify API gem. Run gem install shopify_api"
-puts " * Run ./script/generate shopify_app <api_key> <secret>"
+puts
+puts " * Install the Shopify API gem:
+
+           $ gem install shopify_api"
+puts
+puts " * Run 
+
+           $ rails generate shopify_app your_app_api_key your_app_secret"
+puts
 puts " * Set up a test shop to install your app in (do this on the Partner site)"
-puts " * Run ./script/server"
+puts
+puts " * Run $ rails server"
+puts
 puts " * Visit http://localhost:3000 and use the test shop's URL to install this app"
 puts
