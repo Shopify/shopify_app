dbe2ba368c1be73f4038ab2a74a0a085e490bb1d
Edward Ocampo-Gooding <edward@edwardog.net>
2011-03-08 09:51:13 -0500

    Revert check for API key/secret during railtie init - move to shopify_api

--- lib/shopify_app/railtie.rb
+++ lib/shopify_app/railtie.rb
@@ -1,14 +1,9 @@
 require 'rails'
 
 class ShopifyApp::Railtie < ::Rails::Railtie
+
   config.before_configuration do
     config.shopify = ShopifyApp.configuration
-  end
-  
-  initializer "shopify_app.check_api_credentials_set" do
-    if config.shopify.api_key.blank? || config.shopify.api_key.blank?
-      raise ArgumentError, "Shopify API key/secret not set - please check your config/application.rb"
-    end
   end
   
   initializer "shopify_app.action_controller_integration" do
--- lib/shopify_app/version.rb
+++ lib/shopify_app/version.rb
@@ -1,3 +1,3 @@
 module ShopifyApp
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
