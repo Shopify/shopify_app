2589f57a955a93121150a925d50e2a2a5a09359f
David Underwood <david.underwood@jadedpixel.com>
2012-05-09 14:27:48 -0400

    Modifies login_protection to set up and tear down session

--- lib/shopify_app/login_protection.rb
+++ lib/shopify_app/login_protection.rb
@@ -4,9 +4,11 @@ module ShopifyApp::LoginProtection
       begin
         # session[:shopify] set in LoginController#finalize
         ShopifyAPI::Base.site = session[:shopify].site
+        ShopifyAPI::Base.activate_session(session[:shopify])
         yield
       ensure 
         ShopifyAPI::Base.site = nil
+        ShopifyAPI::Base.clear_session
       end
     else
       session[:return_to] = request.fullpath
