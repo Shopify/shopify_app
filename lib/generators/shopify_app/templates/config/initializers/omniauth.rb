ff5769b8ef0bac9489b3e68f3146a405521fbcd7
David Underwood <david.underwood@jadedpixel.com>
2012-05-11 11:37:23 -0400

    Moved the api scope definition to the config

--- lib/generators/shopify_app/templates/config/initializers/omniauth.rb
+++ lib/generators/shopify_app/templates/config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify, 
            ShopifyApp.configuration.api_key, 
            ShopifyApp.configuration.secret,
-           :scope => 'write_script_tags,write_shipping,write_orders,read_orders,read_products,read_customers,read_content',
+           :scope => ShopifyApp.configuration.scope,
            :setup => lambda {|env| 
                        params = Rack::Utils.parse_query(env['QUERY_STRING'])
                        site_url = "https://#{params['shop']}"
