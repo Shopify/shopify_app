a7d9dc39d81aa810f99e166a40398aaf730d5ebc
Edward Ocampo-Gooding <edward@edwardog.net>
2012-07-19 11:03:53 -0400

    Add example permission scopes

--- lib/generators/shopify_app/templates/config/initializers/omniauth.rb
+++ lib/generators/shopify_app/templates/config/initializers/omniauth.rb
@@ -2,6 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify, 
            ShopifyApp.configuration.api_key, 
            ShopifyApp.configuration.secret,
+
+           # Example permission scopes - see http://api.shopify.com/authentication.html for full listing
+           # :scope => 'read_orders, write_products',
+
            :setup => lambda {|env| 
                        params = Rack::Utils.parse_query(env['QUERY_STRING'])
                        site_url = "https://#{params['shop']}"
