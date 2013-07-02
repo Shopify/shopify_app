2b10ea1abc54cffe5570734fd64c097cff91c24f
Peter McCracken <peter.mccracken@jadedpixel.com>
2013-07-02 14:12:11 -0400

    minimal support for Rails 4

--- lib/generators/shopify_app/shopify_app_generator.rb
+++ lib/generators/shopify_app/shopify_app_generator.rb
@@ -34,8 +34,10 @@ class ShopifyAppGenerator < Rails::Generators::Base
   end
 
   def add_bootstrap_gem
-    insert_into_file "Gemfile", "\ngem 'less-rails-bootstrap'\n\n", :before => '# Gems used only for assets and not required'
-    insert_into_file "Gemfile", "\ngem 'therubyracer', :platforms => :ruby\n\n", :before => 'end'
+    gem_group :development, :test do
+      gem "less-rails-bootstrap"
+      gem 'therubyracer', :platforms => :ruby
+    end
   end
   
   def add_routes
