c1b2b349006d183519bd8033a669c8fca4236e9a
Caroline Hill <mllegeorgesand@gmail.com>
2013-07-08 11:36:30 -0400

    Add logged_in_shop helper

--- lib/generators/shopify_app/templates/app/controllers/home_controller.rb
+++ lib/generators/shopify_app/templates/app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   
   around_filter :shopify_session, :except => 'welcome'
   
+  helper_method :logged_in_shop
+  
   def welcome
     current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
     @callback_url = "http://#{current_host}/login"
@@ -13,6 +15,12 @@ class HomeController < ApplicationController
 
     # get latest 5 orders
     @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 5, :order => "created_at DESC" })
+  end
+  
+  protected
+
+  def logged_in_shop
+    session[:shopify].shop
   end
   
 end
