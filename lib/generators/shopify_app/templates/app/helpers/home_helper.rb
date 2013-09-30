a0f303c4f8dfa1447f469004d388ffe169ce783d
jules2689 <julian.nadeau@shopify.com>
2013-09-30 14:40:13 -0400

    Move to Home Helper

--- lib/generators/shopify_app/templates/app/helpers/home_helper.rb
+++ lib/generators/shopify_app/templates/app/helpers/home_helper.rb
@@ -1,3 +1,9 @@
 module HomeHelper
 
+  def customer_name(order)
+    name = ''
+    name += [order.customer.first_name, order.customer.last_name].join(" ") if order.respond_to?(:customer)
+    name.strip
+  end
+
 end
