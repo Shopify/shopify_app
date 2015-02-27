20dec7193c3a8e1dce6eeac3177de579d28e9744
Kevin Hughes <kevin.hughes@shopify.com>
2015-02-26 21:25:13 -0500

    remove the add to model generator for now, shop model generator tophatted

--- lib/generators/shopify_app/shop_model/shop_model_generator.rb
+++ lib/generators/shopify_app/shop_model/shop_model_generator.rb
@@ -7,46 +7,23 @@ module ShopifyApp
       include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
 
-      def create_or_inject_into_shop_model
-        if File.exist? "app/models/shop.rb"
-          inject_into_file(
-            "app/models/shop.rb",
-            "include ShopifyApp::Shop\n\n",
-            after: "class Shop < ActiveRecord::Base\n"
-          )
-        else
-          copy_file 'shop.rb', 'app/models/shop.rb'
-        end
+      def create_shop_model
+        copy_file 'shop.rb', 'app/models/shop.rb'
       end
 
       def create_shop_migration
-        if shops_table_exists?
-          create_add_columns_migration
-        else
-          copy_migration 'create_shops.rb'
-        end
+        copy_migration 'create_shops.rb'
       end
 
       def create_session_storage
-        copy_file 'session_store.rb', 'app/models/session_storage.rb'
+        copy_file 'session_storage.rb', 'app/models/session_storage.rb'
       end
 
       def create_session_storage_initializer
-        copy_file 'shopify_session_repository.rb', 'config/initializers/shopify_session_repository.rb'
+        copy_file 'shopify_session_repository.rb', 'config/initializers/shopify_session_repository.rb', force: true
       end
 
       private
-
-      def create_add_columns_migration
-        if migration_needed?
-          config = {
-            new_columns: new_columns,
-            new_indexes: new_indexes
-          }
-
-          copy_migration('add_to_shops.rb', config)
-        end
-      end
 
       def copy_migration(migration_name, config = {})
         unless migration_exists?(migration_name)
@@ -56,23 +33,6 @@ module ShopifyApp
             config
           )
         end
-      end
-
-      def migration_needed?
-        new_columns.any? || new_indexes.any?
-      end
-
-      def new_columns
-        @new_columns ||= {
-          shopify_domain: 't.string :shopify_domain',
-          shopify_token: 't.string :shopify_token'
-        }.reject { |column| existing_shops_columns.include?(column.to_s) }
-      end
-
-      def new_indexes
-        @new_indexes ||= {
-          index_shops_on_shopify_domain: 'add_index :shops, :shopify_domain',
-        }.reject { |index| existing_shops_indexes.include?(index.to_s) }
       end
 
       def migration_exists?(name)
@@ -87,18 +47,6 @@ module ShopifyApp
 
       def migration_name_without_timestamp(file)
         file.sub(%r{^.*(db/migrate/)(?:\d+_)?}, '')
-      end
-
-      def shops_table_exists?
-        ActiveRecord::Base.connection.table_exists?(:shops)
-      end
-
-      def existing_shops_columns
-        ActiveRecord::Base.connection.columns(:shops).map(&:name)
-      end
-
-      def existing_shops_indexes
-        ActiveRecord::Base.connection.indexes(:shops).map(&:name)
       end
 
       # for generating a timestamp when using `create_migration`
