d355903d7026041ada5a9891e093a1da7b2ba2d0
Kevin Hughes <kevin.hughes@shopify.com>
2015-04-18 20:37:59 -0400

    fix shopify_app all generator

--- lib/generators/shopify_app/shopify_app_generator.rb
+++ lib/generators/shopify_app/shopify_app_generator.rb
@@ -1,6 +1,16 @@
-generate "shopify_app:install"
-generate "shopify_app:shop_model"
+module ShopifyApp
+  module Generators
+    class ShopifyAppGenerator < Rails::Generators::Base
 
-generate "shopify_app:controllers"
-generate "shopify_app:views"
-generate "shopify_app:routes"
+      def run_all_generators
+        generate "shopify_app:install"
+        generate "shopify_app:shop_model"
+
+        generate "shopify_app:controllers"
+        generate "shopify_app:views"
+        generate "shopify_app:routes"
+      end
+
+    end
+  end
+end
