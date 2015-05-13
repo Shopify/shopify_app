35ba40345afd7d136be9e17bfddc585d193f1f27
Kevin Hughes <kevin.hughes@shopify.com>
2015-05-13 16:23:30 -0400

    draw routes on the app - no need to mount use the routes boolean

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 if ShopifyApp.configuration.routes_enabled?
-  ShopifyApp::Engine.routes.draw do
+  Rails.application.routes.draw do
     controller :sessions do
       get 'login' => :new, :as => :login
       post 'login' => :create, :as => :authenticate
