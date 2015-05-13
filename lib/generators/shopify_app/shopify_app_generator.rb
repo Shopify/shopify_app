ab37aec1c29282f9d5a9b0f7354aa7bcdb864fa3
Kevin Hughes <kevin.hughes@shopify.com>
2015-05-13 16:20:25 -0400

    passes the options through to the install generator

--- lib/generators/shopify_app/shopify_app_generator.rb
+++ lib/generators/shopify_app/shopify_app_generator.rb
@@ -2,8 +2,13 @@ module ShopifyApp
   module Generators
     class ShopifyAppGenerator < Rails::Generators::Base
 
+      def initialize(args, *options)
+        @opts = options.first
+        super(args, *options)
+      end
+
       def run_all_generators
-        generate "shopify_app:install"
+        generate "shopify_app:install #{@opts.join(' ')}"
         generate "shopify_app:shop_model"
 
         generate "shopify_app:controllers"
