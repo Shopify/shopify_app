f4267098771c876490b82547746c1d5b82ed6122
Kevin Hughes <kevin.hughes@shopify.com>
2015-06-23 12:37:00 -0400

    pull layout config to authenticated controller

--- lib/generators/shopify_app/install/templates/home_controller.rb
+++ lib/generators/shopify_app/install/templates/home_controller.rb
@@ -1,10 +1,5 @@
 class HomeController < AuthenticatedController
-<% if embedded_app? -%>
-  layout 'embedded_app'
-<% end -%>
-
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
-
 end
