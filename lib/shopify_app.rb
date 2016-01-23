ba4bc0bbca9286c0f2c9b1ebd370dcb01d3f5a60
John Tajima <manjiro@gmail.com>
2016-01-23 13:26:22 -0500

    Adds scripttagManager and background job to create scripttags when a new shop is added to app, in the same fashion as webhooksManager

--- lib/shopify_app.rb
+++ lib/shopify_app.rb
@@ -19,6 +19,7 @@ require 'shopify_app/session_storage'
 require 'shopify_app/sessions_concern'
 require 'shopify_app/login_protection'
 require 'shopify_app/webhooks_manager'
+require 'shopify_app/scripttags_manager'
 require 'shopify_app/webhook_verification'
 require 'shopify_app/utils'
 
---
+++ test/shopify_app/scripttags_manager_test.rb
@@ -0,0 +1,83 @@
+require 'test_helper'
+
+class ShopifyApp::ScripttagsManagerTest < ActiveSupport::TestCase
+
+  setup do
+    ShopifyApp.configure do |config|
+      config.scripttags = [
+        {event: 'onload', src: 'https://example-app.com/fancy.js'},
+        {event: 'onload', src: 'https://example-app.com/foobar.js'}
+      ]
+    end
+
+    @manager = ShopifyApp::ScripttagsManager.new("regular-shop.myshopify.com", "token")
+  end
+
+  test "#create_scripttags makes calls to create scripttags" do
+    ShopifyAPI::ScriptTag.stubs(all: [])
+
+    expect_scripttag_creation('onload', 'https://example-app.com/fancy.js')
+    expect_scripttag_creation('onload', 'https://example-app.com/foobar.js')
+    @manager.create_scripttags
+  end
+
+  test "#create_scripttags when creating a scripttag fails, raises an error" do
+    ShopifyAPI::ScriptTag.stubs(all: [])
+    scripttag = stub(persisted?: false)
+    ShopifyAPI::ScriptTag.stubs(create: scripttag)
+
+    assert_raise ShopifyApp::ScripttagsManager::CreationFailed do
+      @manager.create_scripttags
+    end
+  end
+
+  test "#create_scripttags when creating a scripttag fails and the scripttag exists, do not raise an error" do
+    scripttag = stub(persisted?: false)
+    scripttags = all_scripttag_srcs.map{|t| stub(src: t)}
+    ShopifyAPI::ScriptTag.stubs(create: scripttag, all: scripttags)
+
+    assert_nothing_raised ShopifyApp::ScripttagsManager::CreationFailed do
+      @manager.create_scripttags
+    end
+  end
+
+  test "#recreate_scripttags! destroys all scripttags and recreates" do
+    @manager.expects(:destroy_scripttags)
+    @manager.expects(:create_scripttags)
+
+    @manager.recreate_scripttags!
+  end
+
+  test "#destroy_scripttags makes calls to destroy scripttags" do
+    ShopifyAPI::ScriptTag.stubs(:all).returns(Array.wrap(all_mock_scripttags.first))
+    ShopifyAPI::ScriptTag.expects(:delete).with(all_mock_scripttags.first.id)
+
+    @manager.destroy_scripttags
+  end
+
+  test "#destroy_scripttags does not destroy scripttags that do not have a matching address" do
+    ShopifyAPI::ScriptTag.stubs(:all).returns([stub(src: 'http://something-or-the-other.com/badscript.js', id: 7214109)])
+    ShopifyAPI::ScriptTag.expects(:delete).never
+
+    @manager.destroy_scripttags
+  end
+
+  private
+
+  def expect_scripttag_creation(event, src)
+    stub_scripttag = stub(persisted?: true)
+    ShopifyAPI::ScriptTag.expects(:create).with(event: event, src: src, format: 'json').returns(stub_scripttag)
+  end
+
+  def all_scripttag_srcs
+    @scripttags ||= ['https://example-app.com/fancy.js', 'https://example-app.com/foobar.js']
+  end
+
+  def all_mock_scripttags
+    [
+      stub(id: 1, src: "https://example-app.com/fancy.js", event: 'onload'),
+      stub(id: 2, src: "https://example-app.com/foobar.js", event: 'onload'),
+      
+    ]
+  end
+end
