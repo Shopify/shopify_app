8bc57fcf15fe6108fc7fee1b7b7f8e3fcb4baf4a
Christian Blais <christ.blais@gmail.com>
2016-04-11 19:57:34 -0400

    Managers are now shopify session agnostic

--- lib/shopify_app/scripttags_manager.rb
+++ lib/shopify_app/scripttags_manager.rb
@@ -2,12 +2,8 @@ module ShopifyApp
   class ScripttagsManager
     class CreationFailed < StandardError; end
 
-    def self.queue(shop_name, token)
-      ShopifyApp::ScripttagsManagerJob.perform_later(shop_name: shop_name, token: token)
-    end
-
-    def initialize(shop_name, token)
-      @shop_name, @token = shop_name, token
+    def self.queue(shop_domain, shop_token)
+      ShopifyApp::ScripttagsManagerJob.perform_later(shop_domain: shop_domain, shop_token: shop_token)
     end
 
     def recreate_scripttags!
@@ -18,19 +14,16 @@ module ShopifyApp
     def create_scripttags
       return unless required_scripttags.present?
 
-      with_shopify_session do
-        required_scripttags.each do |scripttag|
-          create_scripttag(scripttag) unless scripttag_exists?(scripttag[:src])
-        end
+      required_scripttags.each do |scripttag|
+        create_scripttag(scripttag) unless scripttag_exists?(scripttag[:src])
       end
     end
 
     def destroy_scripttags
-      with_shopify_session do
-        ShopifyAPI::ScriptTag.all.each do |scripttag|
-          ShopifyAPI::ScriptTag.delete(scripttag.id) if is_required_scripttag?(scripttag)
-        end
+      ShopifyAPI::ScriptTag.all.each do |scripttag|
+        ShopifyAPI::ScriptTag.delete(scripttag.id) if is_required_scripttag?(scripttag)
       end
+
       @current_scripttags = nil
     end
 
@@ -42,12 +35,6 @@ module ShopifyApp
 
     def is_required_scripttag?(scripttag)
       required_scripttags.map{ |w| w[:src] }.include? scripttag.src
-    end
-
-    def with_shopify_session
-      ShopifyAPI::Session.temp(@shop_name, @token) do
-        yield
-      end
     end
 
     def create_scripttag(attributes)
--- lib/shopify_app/scripttags_manager_job.rb
+++ lib/shopify_app/scripttags_manager_job.rb
@@ -1,11 +1,10 @@
 module ShopifyApp
   class ScripttagsManagerJob < ActiveJob::Base
-    def perform(params = {})
-      shop_name = params.fetch(:shop_name)
-      token = params.fetch(:token)
-
-      manager = ScripttagsManager.new(shop_name, token)
-      manager.create_scripttags
+    def perform(shop_domain:, shop_token:)
+      ShopifyAPI::Session.temp(shop_domain, shop_token) do
+        manager = ScripttagsManager.new
+        manager.create_scripttags
+      end
     end
   end
 end
