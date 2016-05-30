3c66abf1f54f871b474006b4e6d25628af286904
Sai Warang <cyprusad@gmail.com>
2016-05-30 12:47:57 -0400

    Make it possible to override shopify_token validation

--- lib/shopify_app/shop.rb
+++ lib/shopify_app/shop.rb
@@ -4,11 +4,19 @@ module ShopifyApp
 
     included do
       validates :shopify_domain, presence: true, uniqueness: true
-      validates :shopify_token, presence: true
+      if shopify_token_needs_to_exist?
+        validates :shopify_token, presence: true
+      end
     end
 
     def with_shopify_session(&block)
       ShopifyAPI::Session.temp(shopify_domain, shopify_token, &block)
+    end
+
+    module ClassMethods
+      def shopify_token_needs_to_exist?
+        true
+      end
     end
 
   end
