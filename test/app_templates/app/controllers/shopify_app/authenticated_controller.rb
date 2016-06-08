3592f6c5c5995b051398039a6475b0c851850355
Christian Blais <christ.blais@gmail.com>
2016-06-08 13:51:11 -0400

    Revert "Create shop helper with shop model generation"

--- test/app_templates/app/controllers/shopify_app/authenticated_controller.rb
+++
@@ -1,7 +0,0 @@
-module ShopifyApp
-  class AuthenticatedController < ApplicationController
-    before_action :login_again_if_different_shop
-    around_filter :shopify_session
-    layout ShopifyApp.configuration.embedded_app? ? 'embedded_app' : 'application'
-  end
-end
