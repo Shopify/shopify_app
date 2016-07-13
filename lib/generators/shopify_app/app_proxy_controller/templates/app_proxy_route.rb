980db6478eeedd0ac30cd1e43dd8262c8e12abec
ashmaroli <ashmaroli@users.noreply.github.com>
2016-07-13 20:56:36 +0530

    Create app_proxy_route.rb

---
+++ lib/generators/shopify_app/app_proxy_controller/templates/app_proxy_route.rb
@@ -0,0 +1,9 @@
+  namespace :app_proxy do
+    root action: 'index'
+    # simple routes without a specified controller will go to AppProxyController
+    
+    # more complex routes will go to controllers in the AppProxy namespace
+    # 	resources :reviews
+    # GET /app_proxy/reviews will now be routed to
+    # AppProxy::ReviewsController#index, for example
+  end
