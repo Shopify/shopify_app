8e2c5672f72ff4dac242025e5e5f4ebb321a1715
ashmaroli <ashmaroli@users.noreply.github.com>
2016-07-19 23:59:02 +0530

    Revert "App proxy"

--- lib/generators/shopify_app/app_proxy_controller/templates/app_proxy_controller.rb
+++
@@ -1,8 +0,0 @@
-class AppProxyController < ApplicationController
-   include ShopifyApp::AppProxyVerification
-
-  def index
-    render layout: false, content_type: 'application/liquid'
-  end
-
-end
