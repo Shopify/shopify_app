f8467a9599f611e44ccde81c91e9c54e64ed482f
Sebastian Szturo <sebastian.szturo@gmail.com>
2017-05-02 23:09:20 -0400

    Add i18n locale param support

--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -1,8 +1,12 @@
 module ShopifyApp
   class AuthenticatedController < ApplicationController
+    include ShopifyApp::Localization
     include ShopifyApp::LoginProtection
+
+    before_action :set_locale
     before_action :login_again_if_different_shop
     around_action :shopify_session
+
     layout ShopifyApp.configuration.embedded_app? ? 'embedded_app' : 'application'
   end
 end
