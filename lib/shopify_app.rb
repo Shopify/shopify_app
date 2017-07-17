5926281564809837e25084c4c941a66b305863e3
Kevin Hughes <kevinhughes27@gmail.com>
2017-07-17 14:13:30 -0400

    organize include file

--- lib/shopify_app.rb
+++ lib/shopify_app.rb
@@ -10,20 +10,22 @@ require 'shopify_app/configuration'
 # engine
 require 'shopify_app/engine'
 
-# jobs
-require 'shopify_app/webhooks_manager_job'
-require 'shopify_app/scripttags_manager_job'
-
 # helpers and concerns
 require 'shopify_app/shop'
 require 'shopify_app/session_storage'
 require 'shopify_app/localization'
 require 'shopify_app/login_protection'
-require 'shopify_app/webhooks_manager'
-require 'shopify_app/scripttags_manager'
 require 'shopify_app/webhook_verification'
 require 'shopify_app/app_proxy_verification'
 require 'shopify_app/utils'
+
+# jobs
+require 'shopify_app/webhooks_manager_job'
+require 'shopify_app/scripttags_manager_job'
+
+# managers
+require 'shopify_app/webhooks_manager'
+require 'shopify_app/scripttags_manager'
 
 # session repository
 require 'shopify_app/shopify_session_repository'
