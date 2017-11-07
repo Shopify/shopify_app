9d81d25f7d57675de5ecab7fc6c564ca01370836
Shayne Parmelee <shayne@shopify.com>
2017-11-07 14:13:13 -0500

    Add CSRF Protection on AuthenticatedController

--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -4,6 +4,7 @@ module ShopifyApp
     include ShopifyApp::LoginProtection
     include ShopifyApp::EmbeddedApp
 
+    protect_from_forgery with: :exception
     before_action :login_again_if_different_shop
     around_action :shopify_session
   end
