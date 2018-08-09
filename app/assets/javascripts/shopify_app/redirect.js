28bf0580031b39c4bbfae10d796516e69271f7c4
Francine Navarro <francine.navarro@shopify.com>
2018-08-09 10:38:29 -0400

    Attempts to prevent unnecessary redirects

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -3,20 +3,40 @@ document.addEventListener("DOMContentLoaded", function() {
   var targetInfo = JSON.parse(redirectTargetElement.dataset.target);
 
   function redirect() {
+    document.cookie = 'can_use_cookies=true';
     window.top.location.href = targetInfo.url;
   }
 
   if (window.top == window.self) {
     // If Storage Access API is available, generate UI for user to interact with to trigger cookie partitioning
-    if (document.hasStorageAccess) {
-      const button = document.createElement('button');
-      button.innerHTML = 'Request Storage Access';
-      button.addEventListener('click', redirect);
-      document.body.appendChild(button);
+    if (!document.hasStorageAccess) {
+      redirect();
       return;
     }
 
-    redirect();   
+    // Prevents second redirect pre-partition
+    // if (!document.cookie) {
+    //   const button = document.createElement('button');
+    //   button.innerHTML = 'Request Storage Access';
+    //   button.addEventListener('click', redirect);
+    //   document.body.appendChild(button);
+    // } else {
+    //   redirect();
+    // }
+
+    // Prevents button view post-partition
+    // Causes infinite loop pre-partition (hasAccess is true?)
+    // document.hasStorageAccess().then((hasAccess) => {
+    //   if (hasAccess) {
+    //     redirect();
+    //     return;
+    //   }
+
+    //   const button = document.createElement('button');
+    //   button.innerHTML = 'Request Storage Access';
+    //   button.addEventListener('click', redirect);
+    //   document.body.appendChild(button);
+    // });
   } else {
     // If the current window is the 'child', change the parent's URL with postMessage
     normalizedLink = document.createElement('a');
