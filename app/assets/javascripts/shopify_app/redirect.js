250fc21b25b6c34ed9356e80586c1058c6617e70
Francine Navarro <francine.navarro@shopify.com>
2018-08-09 13:04:47 -0400

    WIP

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var redirectTargetElement = document.getElementById("redirection-target");
   var targetInfo = JSON.parse(redirectTargetElement.dataset.target);
 
+  var CAN_USE_COOKIES = 'can_use_cookies=true';
+
   function redirect() {
-    document.cookie = 'can_use_cookies=true';
+    document.cookie = CAN_USE_COOKIES;
     window.top.location.href = targetInfo.url;
   }
 
@@ -14,18 +16,23 @@ document.addEventListener("DOMContentLoaded", function() {
       return;
     }
 
-    // Prevents second redirect pre-partition
-    // if (!document.cookie) {
-    //   const button = document.createElement('button');
-    //   button.innerHTML = 'Request Storage Access';
-    //   button.addEventListener('click', redirect);
-    //   document.body.appendChild(button);
-    // } else {
-    //   redirect();
-    // }
+    // Pre-partition: Prevents second redirect to button page (still does two redirects to app as TLD)
+    // Post-partition: document.cookie doesn't persist; still redirects once to button page
+    var allCookies = document.cookie;
+    var shouldRedirect = allCookies.search(CAN_USE_COOKIES);
 
-    // Prevents button view post-partition
-    // Causes infinite loop pre-partition (hasAccess is true?)
+    if (shouldRedirect < 0) {
+      const button = document.createElement('button');
+      button.innerHTML = 'Request Storage Access';
+      button.addEventListener('click', redirect);
+      document.body.appendChild(button);
+    } else {
+      redirect();
+    }
+
+    // Buggy
+    // Pre-partition: Causes infinite redirects (hasAccess is true?)
+    // Post-partition: Prevents redirect to button page, but embedded app does not always load
     // document.hasStorageAccess().then((hasAccess) => {
     //   if (hasAccess) {
     //     redirect();
