f9ee88b824e5203963a90ac17b2de407eee8e709
Mike Ragalie <mike.ragalie@shopify.com>
2018-09-04 23:54:51 -0400

    IIFE

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -1,31 +1,33 @@
-function redirect() {
-  var redirectTargetElement = document.getElementById("redirection-target");
+(function() {
+  function redirect() {
+    var redirectTargetElement = document.getElementById("redirection-target");
 
-  if (!redirectTargetElement) {
-    return;
+    if (!redirectTargetElement) {
+      return;
+    }
+
+    var targetInfo = JSON.parse(redirectTargetElement.dataset.target)
+
+    if (window.top == window.self) {
+      // If the current window is the 'parent', change the URL by setting location.href
+      window.top.location.href = targetInfo.url;
+    } else {
+      // If the current window is the 'child', change the parent's URL with postMessage
+      normalizedLink = document.createElement('a');
+      normalizedLink.href = targetInfo.url;
+
+      data = JSON.stringify({
+        message: 'Shopify.API.remoteRedirect',
+        data: {location: normalizedLink.href}
+      });
+      window.parent.postMessage(data, targetInfo.myshopifyUrl);
+    }
   }
 
-  var targetInfo = JSON.parse(redirectTargetElement.dataset.target)
+  document.addEventListener("DOMContentLoaded", redirect);
 
-  if (window.top == window.self) {
-    // If the current window is the 'parent', change the URL by setting location.href
-    window.top.location.href = targetInfo.url;
-  } else {
-    // If the current window is the 'child', change the parent's URL with postMessage
-    normalizedLink = document.createElement('a');
-    normalizedLink.href = targetInfo.url;
-
-    data = JSON.stringify({
-      message: 'Shopify.API.remoteRedirect',
-      data: { location: normalizedLink.href }
-    });
-    window.parent.postMessage(data, targetInfo.myshopifyUrl);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", redirect);
-
-// In the turbolinks context, neither DOMContentLoaded nor turbolinks:load
-// consistently fires. This ensures that we at least attempt to fire in the
-// turbolinks situation as well.
-redirect();
+  // In the turbolinks context, neither DOMContentLoaded nor turbolinks:load
+  // consistently fires. This ensures that we at least attempt to fire in the
+  // turbolinks situation as well.
+  redirect();
+})();
