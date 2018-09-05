5511602fa1faa5a2ecb3230ccc8f25c0cc0a9208
Terence Li <terence.li@shopify.com>
2018-09-05 13:13:03 -0400

    Removing read_orders scope and adding comment to scope docs

--- lib/generators/shopify_app/install/templates/shopify_app.rb
+++ lib/generators/shopify_app/install/templates/shopify_app.rb
@@ -2,7 +2,8 @@ ShopifyApp.configure do |config|
   config.application_name = "<%= @application_name %>"
   config.api_key = "<%= @api_key %>"
   config.secret = "<%= @secret %>"
-  config.scope = "<%= @scope %>"
+  config.scope = "<%= @scope %>" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = <%= embedded_app? %>
   config.after_authenticate_job = false
   config.session_repository = ShopifyApp::InMemorySessionStore
