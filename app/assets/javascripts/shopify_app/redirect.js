a64ec744cafb57d3a6fad649011cc49c1b8df210
Francine Navarro <francine.navarro@shopify.com>
2018-09-11 16:15:52 -0400

    Experiement with STP Release 65

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -8,11 +8,7 @@
 
     var targetInfo = JSON.parse(redirectTargetElement.dataset.target)
 
-    if (window.top == window.self) {
-      // If the current window is the 'parent', change the URL by setting location.href
-      window.top.location.href = targetInfo.url;
-    } else {
-      // If the current window is the 'child', change the parent's URL with postMessage
+    function redirectViaPostMessage() {
       normalizedLink = document.createElement('a');
       normalizedLink.href = targetInfo.url;
 
@@ -21,6 +17,33 @@
         data: {location: normalizedLink.href}
       });
       window.parent.postMessage(data, targetInfo.myshopifyUrl);
+    }
+
+    if (window.top == window.self) {
+      // If the current window is the 'parent', change the URL by setting location.href
+      window.top.location.href = targetInfo.url;
+    } else {
+      // If the current window is the 'child', change the parent's URL with postMessage
+      document.hasStorageAccess().then((hasAccess) => {
+        if (hasAccess) {
+          // Can't get this to be true in the iframe
+          redirectViaPostMessage();
+        } else {
+          // How can we tell if we need to request for storage access or not?
+          const requestButton = document.createElement('button');
+          requestButton.innerHTML = 'Give me access';
+          requestButton.addEventListener('click', () => {
+            document.requestStorageAccess().then(() => {
+              redirectViaPostMessage();
+            }, () => {
+              // needed for first redirect, if user has not interacted with TLD
+              redirectViaPostMessage();
+            });
+          });
+    
+          document.body.appendChild(requestButton);
+        }
+      });
     }
   }
 
