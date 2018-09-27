acf41867701fa1da2d5fde958a42ca8fead2b3cf
Francine Navarro <francine.navarro@shopify.com>
2018-09-26 22:11:26 -0400

    Add node_modules/ to gitignore

--- node_modules/babel-preset-shopify/flow.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    require.resolve('babel-plugin-syntax-flow'),
-    require.resolve('babel-plugin-transform-flow-strip-types'),
-  ],
-};
--- node_modules/karma-chai-sinon/adapter.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-  window.should = null; // Workaround for "RangeError: Maximum call stack size exceeded." in PhantomJS
-  window.should = window.chai.should();
-  window.expect = window.chai.expect;
-  window.assert = window.chai.assert;
-})(window);
