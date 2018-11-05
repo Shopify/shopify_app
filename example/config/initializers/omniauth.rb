cc71158d330e66a2ada45726de72199edec807f0
Aditya Mattos <aditya.mattos@shopify.com>
2018-11-05 13:19:36 -0500

    Policial fixes

--- example/config/initializers/omniauth.rb
+++ example/config/initializers/omniauth.rb
@@ -2,10 +2,10 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
-   ShopifyApp.configuration.api_key,
-   ShopifyApp.configuration.secret,
-   scope: ShopifyApp.configuration.scope,
-   setup: lambda do |env|
+           ShopifyApp.configuration.api_key,
+           ShopifyApp.configuration.secret,
+           scope: ShopifyApp.configuration.scope,
+           setup: lambda do |env|
     strategy = env['omniauth.strategy']
 
     shopify_auth_params = strategy.session['shopify.omniauth_params']&.with_indifferent_access
