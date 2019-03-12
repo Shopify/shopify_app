35a601ceb2db78558ef770cec3b95242f77624af
Jipsica <jessica.ip@shopify.com>
2019-03-12 13:17:21 -0400

    Include session_store template in the install generator (#704)
    * updated generator to use session_store template with persisted cookies

---
+++ lib/generators/shopify_app/install/templates/session_store.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store(:cookie_store, key: '_example_session', expire_after: 14.days)
