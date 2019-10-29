8f834508603725744d94594c565995414131210a
Tim Anema <tim.anema@shopify.com>
2019-10-29 13:28:26 -0400

    Changing install generator to allow rails 6 to use webpack

--- lib/shopify_app.rb
+++ lib/shopify_app.rb
@@ -4,32 +4,38 @@ require 'shopify_app/version'
 require 'shopify_api'
 require 'omniauth-shopify-oauth2'
 
-# config
-require 'shopify_app/configuration'
+module ShopifyApp
+  def self.rails6?
+    Rails::VERSION::MAJOR >= 6
+  end
 
-# engine
-require 'shopify_app/engine'
+  # config
+  require 'shopify_app/configuration'
 
-# utils
-require 'shopify_app/utils'
+  # engine
+  require 'shopify_app/engine'
 
-# controller concerns
-require 'shopify_app/controller_concerns/localization'
-require 'shopify_app/controller_concerns/itp'
-require 'shopify_app/controller_concerns/login_protection'
-require 'shopify_app/controller_concerns/embedded_app'
-require 'shopify_app/controller_concerns/webhook_verification'
-require 'shopify_app/controller_concerns/app_proxy_verification'
+  # utils
+  require 'shopify_app/utils'
 
-# jobs
-require 'shopify_app/jobs/webhooks_manager_job'
-require 'shopify_app/jobs/scripttags_manager_job'
+  # controller concerns
+  require 'shopify_app/controller_concerns/localization'
+  require 'shopify_app/controller_concerns/itp'
+  require 'shopify_app/controller_concerns/login_protection'
+  require 'shopify_app/controller_concerns/embedded_app'
+  require 'shopify_app/controller_concerns/webhook_verification'
+  require 'shopify_app/controller_concerns/app_proxy_verification'
 
-# managers
-require 'shopify_app/managers/webhooks_manager'
-require 'shopify_app/managers/scripttags_manager'
+  # jobs
+  require 'shopify_app/jobs/webhooks_manager_job'
+  require 'shopify_app/jobs/scripttags_manager_job'
 
-# session
-require 'shopify_app/session/session_storage'
-require 'shopify_app/session/session_repository'
-require 'shopify_app/session/in_memory_session_store'
+  # managers
+  require 'shopify_app/managers/webhooks_manager'
+  require 'shopify_app/managers/scripttags_manager'
+
+  # session
+  require 'shopify_app/session/session_storage'
+  require 'shopify_app/session/session_repository'
+  require 'shopify_app/session/in_memory_session_store'
+end
