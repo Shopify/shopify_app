31b8f06f845cc7113cca1fb4216ec199a10b1621
Tim Anema <tim.anema@shopify.com>
2020-02-04 11:20:02 -0500

    Adding in proper user agent checking for same site cookie checks on storage access

--- test/javascripts/shopify_app/storage_access_test.js
+++ test/javascripts/shopify_app/storage_access_test.js
@@ -44,6 +44,7 @@ suite('StorageAccessHelper', () => {
     });
 
     test('calls redirectToAppTargetUrl instead of manageStorageAccess or setUpCookiePartitioningStub if ITPHelper.userAgentIsAffected returns true', async () => {
+      storageAccessHelperSandbox.stub(storageAccessHelper, 'sameSiteNoneIncompatible').callsFake(() => true);
       storageAccessHelperSandbox.stub(ITPHelper.prototype, 'userAgentIsAffected').callsFake(() => false);
 
       storageAccessHelperSandbox.stub(storageAccessHelper, 'manageStorageAccess').callsFake(() => true);
@@ -247,8 +248,34 @@ suite('StorageAccessHelper', () => {
     });
   });
 
+  suite('sameSiteNoneIncompatible', () => {
+    test('matches the correct user agents', () => {
+      var incompatibleUserAgents = [
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/87.0.279142407 Mobile/15E148 Safari/605.1",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15",
+        "Mozilla/5.0 (Linux; U; Android 7.0; en-US; SM-G935F Build/NRD90M) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.3.8.976 U3/0.8.0 Mobile Safari/534.30",
+      ]
+      for (var i = 0; i < incompatibleUserAgents.length; i++) {
+        sinon.assert.match(true, storageAccessHelper.sameSiteNoneIncompatible(incompatibleUserAgents[i]))
+      }
+    });
+
+    test('doesnt match the same shite agents', () => {
+      var compatibleUserAgents = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15",
+      ]
+
+      for (var i = 0; i < compatibleUserAgents.length; i++) {
+        sinon.assert.match(false, storageAccessHelper.sameSiteNoneIncompatible(compatibleUserAgents[i]))
+      }
+    });
+  })
+
   suite('setCookieAndRedirect', () => {
     test('sets the shopify.cookies_persist cookie', () => {
+      storageAccessHelperSandbox.stub(storageAccessHelper, 'sameSiteNoneIncompatible').callsFake(() => true);
       storageAccessHelper.setCookieAndRedirect();
       sinon.assert.match(document.cookie.match('shopify.cookies_persist').length, 1);
     });
