a49666f0e5399302f928eca2b38736ba472c5d07
Mike Ragalie <mike.ragalie@shopify.com>
2020-02-25 10:54:03 -0500

    Implement UserSessionStore#retrieve_by_jwt

--- test/shopify_app/session/user_session_storage_test.rb
+++ test/shopify_app/session/user_session_storage_test.rb
@@ -6,11 +6,11 @@ end
 
 module ShopifyApp
   class UserSessionStorageTest < ActiveSupport::TestCase
-    test "tests that session store can retrieve user session records" do
-      TEST_SHOPIFY_USER_ID = 42
-      TEST_SHOPIFY_DOMAIN = "example.myshopify.com"
-      TEST_SHOPIFY_USER_TOKEN = "some-user-token-42"
+    TEST_SHOPIFY_USER_ID = 42
+    TEST_SHOPIFY_DOMAIN = "example.myshopify.com"
+    TEST_SHOPIFY_USER_TOKEN = "some-user-token-42"
 
+    test "tests that session store can retrieve user session records by ID" do
       UserMockSessionStore.stubs(:find_by).returns(MockUserInstance.new(
         shopify_user_id:TEST_SHOPIFY_USER_ID,
         shopify_domain:TEST_SHOPIFY_DOMAIN,
@@ -21,6 +21,28 @@ module ShopifyApp
 
       assert_equal TEST_SHOPIFY_DOMAIN, session.domain
       assert_equal TEST_SHOPIFY_USER_TOKEN, session.token
+    end
+
+    test "tests that session store can retrieve user session records by JWT" do
+      instance = MockUserInstance.new(
+        shopify_user_id: TEST_SHOPIFY_USER_ID,
+        shopify_domain: TEST_SHOPIFY_DOMAIN,
+        shopify_token: TEST_SHOPIFY_USER_TOKEN,
+        api_version: '2020-01',
+      )
+      UserMockSessionStore.stubs(:find_by).with(shopify_user_id: TEST_SHOPIFY_USER_ID).returns(instance)
+
+      expected_session = ShopifyAPI::Session.new(
+        domain: instance.shopify_domain,
+        token: instance.shopify_token,
+        api_version: instance.api_version,
+      )
+
+      payload = { 'sub' => TEST_SHOPIFY_USER_ID }
+      session = UserMockSessionStore.retrieve_by_jwt(payload)
+      assert_equal expected_session.domain, session.domain
+      assert_equal expected_session.token, session.token
+      assert_equal expected_session.api_version, session.api_version
     end
 
     test "tests that session store can store user session records" do
