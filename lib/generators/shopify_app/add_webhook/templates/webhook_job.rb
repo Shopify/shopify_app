b8a5abcc6ea25eab3d9e1de08e51a10c7a7be734
A.D.Faris <adfaris@gmail.com>
2020-04-13 15:45:40 -0400

    Add a logic to exit if a shop domain does not exist

--- lib/generators/shopify_app/add_webhook/templates/webhook_job.rb
+++ lib/generators/shopify_app/add_webhook/templates/webhook_job.rb
@@ -2,6 +2,11 @@ class <%= @job_class_name %> < ActiveJob::Base
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
 
+    if shop.nil?
+      logger.error("#{self.class} failed: cannot find shop with domain '#{shop_domain}'")
+      return
+    end
+
     shop.with_shopify_session do
     end
   end
