5304bc2c1dd1ec985e88b59c968a62be76014ec4
A.D.Faris <adfaris@gmail.com>
2020-04-15 13:09:19 -0400

    Add a .blank? to test to check if query is empty or not

--- lib/shopify_app/controller_concerns/login_protection.rb
+++ lib/shopify_app/controller_concerns/login_protection.rb
@@ -108,7 +108,7 @@ module ShopifyApp
           path = referer.path
           query = "#{referer.query}&#{sanitized_params.to_query}"
         end
-        session[:return_to] = (query) ? "#{path}?#{query}" : "#{path}"
+        session[:return_to] = query.blank? ? "#{path}" : "#{path}?#{query}"
         redirect_to(login_url_with_optional_shop)
       end
     end
