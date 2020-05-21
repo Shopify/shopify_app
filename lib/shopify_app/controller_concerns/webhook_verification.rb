d992046508adc6b7b5e70eb754ee5078467ae254
A.D.Faris <adfaris@gmail.com>
2020-05-21 17:11:04 -0400

    Modified to reuse the HMAC security

--- lib/shopify_app/controller_concerns/webhook_verification.rb
+++ lib/shopify_app/controller_concerns/webhook_verification.rb
@@ -2,6 +2,7 @@
 module ShopifyApp
   module WebhookVerification
     extend ActiveSupport::Concern
+    include PayloadVerification
 
     included do
       skip_before_action :verify_authenticity_token, raise: false
@@ -14,26 +15,8 @@ module ShopifyApp
       data = request.raw_post
       return head(:unauthorized) unless hmac_valid?(data)
     end
-
-    def hmac_valid?(data)
-      secrets = [ShopifyApp.configuration.secret, ShopifyApp.configuration.old_secret].reject(&:blank?)
-
-      secrets.any? do |secret|
-        digest = OpenSSL::Digest.new('sha256')
-
-        ActiveSupport::SecurityUtils.secure_compare(
-          shopify_hmac,
-          Base64.strict_encode64(OpenSSL::HMAC.digest(digest, secret, data))
-        )
-      end
-    end
-
     def shop_domain
       request.headers['HTTP_X_SHOPIFY_SHOP_DOMAIN']
-    end
-
-    def shopify_hmac
-      request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
     end
   end
 end
