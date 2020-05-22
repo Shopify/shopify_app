f5c984a847ead99035a30880852c3c3c2fe03d95
A.D.Faris <adfaris@gmail.com>
2020-05-21 22:40:52 -0400

    Add payloadVerification module to webhookVerifcation

--- lib/shopify_app/controller_concerns/webhook_verification.rb
+++ lib/shopify_app/controller_concerns/webhook_verification.rb
@@ -2,7 +2,7 @@
 module ShopifyApp
   module WebhookVerification
     extend ActiveSupport::Concern
-    include PayloadVerification
+    include ShopifyApp::PayloadVerification
 
     included do
       skip_before_action :verify_authenticity_token, raise: false
@@ -15,6 +15,7 @@ module ShopifyApp
       data = request.raw_post
       return head(:unauthorized) unless hmac_valid?(data)
     end
+
     def shop_domain
       request.headers['HTTP_X_SHOPIFY_SHOP_DOMAIN']
     end
