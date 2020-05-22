a641b318b4115fb0ac3ce965db9eac5e34b26dd5
A.D.Faris <adfaris@gmail.com>
2020-05-22 13:33:51 -0400

    payloadVerification with the updated content

--- lib/shopify_app/controller_concerns/payload_verification.rb
+++ lib/shopify_app/controller_concerns/payload_verification.rb
@@ -19,5 +19,25 @@ module ShopifyApp
     def shopify_hmac
       request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
     end
+
+    def verify_request
+      hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
+      request_body = request.body.read
+      secret = ShopifyApp.configuration.secret
+      digest = OpenSSL::Digest.new('sha256')
+
+      expected_hmac = Base64.strict_encode64(OpenSSL::HMAC.digest(digest, secret, request_body))
+      head(:unauthorized) unless ActiveSupport::SecurityUtils.secure_compare(expected_hmac, hmac_header)
+    end
+
+    def calculated_signature(query_hash_without_signature)
+      sorted_params = query_hash_without_signature.collect { |k, v| "#{k}=#{Array(v).join(',')}" }.sort.join
+
+      OpenSSL::HMAC.hexdigest(
+        OpenSSL::Digest.new('sha256'),
+        ShopifyApp.configuration.secret,
+        sorted_params
+      )
+    end
   end
 end
