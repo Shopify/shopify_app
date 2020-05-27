fae33b7dbd871988a41bad31eda62d5fb4542199
A.D.Faris <adfaris@gmail.com>
2020-05-27 09:36:40 -0400

    refactor to simplify the files shared

--- lib/shopify_app/controller_concerns/payload_verification.rb
+++ lib/shopify_app/controller_concerns/payload_verification.rb
@@ -3,41 +3,21 @@ module ShopifyApp
   module PayloadVerification
     extend ActiveSupport::Concern
 
+    def shopify_hmac
+      request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
+    end
+
     def hmac_valid?(data)
       secrets = [ShopifyApp.configuration.secret, ShopifyApp.configuration.old_secret].reject(&:blank?)
 
       secrets.any? do |secret|
         digest = OpenSSL::Digest.new('sha256')
-
         ActiveSupport::SecurityUtils.secure_compare(
+          # request.headers['HTTP_X_SHOPIFY_HMAC_SHA256'],
           shopify_hmac,
           Base64.strict_encode64(OpenSSL::HMAC.digest(digest, secret, data))
         )
       end
-    end
-
-    def shopify_hmac
-      request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
-    end
-
-    def verify_request
-      hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
-      request_body = request.body.read
-      secret = ShopifyApp.configuration.secret
-      digest = OpenSSL::Digest.new('sha256')
-
-      expected_hmac = Base64.strict_encode64(OpenSSL::HMAC.digest(digest, secret, request_body))
-      head(:unauthorized) unless ActiveSupport::SecurityUtils.secure_compare(expected_hmac, hmac_header)
-    end
-
-    def calculated_signature(query_hash_without_signature)
-      sorted_params = query_hash_without_signature.collect { |k, v| "#{k}=#{Array(v).join(',')}" }.sort.join
-
-      OpenSSL::HMAC.hexdigest(
-        OpenSSL::Digest.new('sha256'),
-        ShopifyApp.configuration.secret,
-        sorted_params
-      )
     end
   end
 end
