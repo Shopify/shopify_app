a21db928321f929d59b1132fc6e8527087431994
Melanie Wang <melanie.wang@shopify.com>
2020-11-19 11:27:13 -0500

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_WARN,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true,
     client: {
       mocha: {
