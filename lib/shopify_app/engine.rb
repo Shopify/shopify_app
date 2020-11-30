e378fc590e2f27042f71570bb5528cb187b0b05d
Jeff Seifert <jeffse@gmail.com>
2020-11-29 22:46:08 -0500

    Test to see if assets are in the configuration

--- lib/shopify_app/engine.rb
+++ lib/shopify_app/engine.rb
@@ -15,13 +15,15 @@ module ShopifyApp
     isolate_namespace ShopifyApp
 
     initializer "shopify_app.assets.precompile" do |app|
-      app.config.assets.precompile += %w[
-        shopify_app/redirect.js
-        shopify_app/top_level.js
-        shopify_app/enable_cookies.js
-        shopify_app/request_storage_access.js
-        storage_access.svg
-      ]
+      if app.config.respond_to?(:assets)
+        app.config.assets.precompile += %w[
+          shopify_app/redirect.js
+          shopify_app/top_level.js
+          shopify_app/enable_cookies.js
+          shopify_app/request_storage_access.js
+          storage_access.svg
+        ]
+      end
     end
 
     initializer "shopify_app.middleware" do |app|
