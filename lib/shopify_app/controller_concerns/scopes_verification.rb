9fc93de054a52cef5fc6e4fb7a2c55661c4ca01a
Rezaan Syed <rezaan.syed@shopify.com>
2021-01-29 17:06:45 -0500

    ScopeVerification for handling scope mismatches

---
+++ lib/shopify_app/controller_concerns/scopes_verification.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module ShopifyApp
+  module ScopesVerification
+    def login_on_scope_changes(current_merchant_scopes, configuration_scopes)
+      redirect_to(shop_login) if scopes_configuration_mismatch?(current_merchant_scopes, configuration_scopes)
+    end
+
+    def scopes_configuration_mismatch?(current_merchant_scopes, configuration_scopes)
+      ShopifyAPI::ApiAccess.new(current_merchant_scopes) != ShopifyAPI::ApiAccess.new(configuration_scopes)
+    end
+
+    private
+
+    def shop_login
+      ShopifyApp::Utils.shop_login_url(shop: params[:shop], return_to: request.fullpath)
+    end
+  end
+end
