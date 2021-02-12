212078e20b7c2c0ec198f702ced8ca67f12533c7
Rezaan Syed <rezaan.syed@shopify.com>
2021-02-12 10:42:41 -0500

    Update shop_generator to add migration for access_scopes

--- lib/generators/shopify_app/shop_model/shop_model_generator.rb
+++ lib/generators/shopify_app/shop_model/shop_model_generator.rb
@@ -16,6 +16,23 @@ module ShopifyApp
         migration_template('db/migrate/create_shops.erb', 'db/migrate/create_shops.rb')
       end
 
+      def create_shop_with_access_scopes_migration
+        scopes_column_prompt = <<~PROMPT
+          It is highly recommended that apps record the access scopes granted by \
+          merchants during app installation. See app/models/shop.rb to modify how \
+          access scopes are stored and retrieved.
+ 
+          [WARNING] Your app will fail the ScopeVerification concern if access scopes are not stored.
+
+          The following migration will add an `access_scopes` column to the Shop model. \
+          Do you want to include this migration? [y/n]
+        PROMPT
+
+        if yes?(scopes_column_prompt)
+          migration_template('db/migrate/add_scopes_column.erb', 'db/migrate/add_scopes_column.rb')
+        end
+      end
+
       def update_shopify_app_initializer
         gsub_file('config/initializers/shopify_app.rb', 'ShopifyApp::InMemoryShopSessionStore', 'Shop')
       end
