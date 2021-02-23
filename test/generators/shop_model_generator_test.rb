f6d134378c2eb17c0d5c65c1ae6e8dbf0e40a771
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-02-23 13:51:23 -0500

    Add tests covering --new-shopify-cli-app

--- test/generators/shop_model_generator_test.rb
+++ test/generators/shop_model_generator_test.rb
@@ -34,6 +34,17 @@ class ShopModelGeneratorTest < Rails::Generators::TestCase
     end
   end
 
+  test "create shop with access_scopes migration with --new-shopify-cli-app flag provided" do
+    Rails.env = 'mock_environment'
+
+    run_generator %w(--new-shopify-cli-app)
+    Rails.env = 'test' # Change this back for subsequent tests
+
+    assert_migration "db/migrate/add_shop_access_scopes_column.rb" do |migration|
+      assert_match "add_column :shops, :access_scopes, :string", migration
+    end
+  end
+
   test "updates the shopify_app initializer" do
     run_generator
     assert_file "config/initializers/shopify_app.rb" do |file|
