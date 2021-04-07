bc4e270c9a19f7010c5b903187d887baee25e8e4
Hanna Chen <hanna.chen@shopify.com>
2021-04-07 14:30:31 -0400

    introduce slim app-bridge client for redirect

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -1,8 +1,11 @@
+//= require ./app_bridge_redirect_1.30.0.js
+
 (function() {
   function redirect() {
     var redirectTargetElement = document.getElementById("redirection-target");
+    var bodyElement = document.getElementsByTagName('body')[0];
 
-    if (!redirectTargetElement) {
+    if (!redirectTargetElement || !bodyElement) {
       return;
     }
 
@@ -12,15 +15,23 @@
       // If the current window is the 'parent', change the URL by setting location.href
       window.top.location.href = targetInfo.url;
     } else {
-      // If the current window is the 'child', change the parent's URL with postMessage
+      // If the current window is the 'child' or embedded, change the parent's URL with App Bridge redirect
+      // This case happens when the app is installed but the app does not know that because there's no shop entry
       normalizedLink = document.createElement('a');
       normalizedLink.href = targetInfo.url;
 
-      data = JSON.stringify({
-        message: 'Shopify.API.remoteRedirect',
-        data: {location: normalizedLink.href}
+      var AppBridge = window['app-bridge'];
+      var actions = window['app-bridge'].actions;
+      var apiKey = bodyElement.dataset.apiKey;
+      var urlParams = new URLSearchParams(window.location.search);
+
+      var app = AppBridge.createApp({
+        apiKey: apiKey,
+        shopOrigin: urlParams.get('shop'),
       });
-      window.parent.postMessage(data, targetInfo.myshopifyUrl);
+
+      var redirect = actions.Redirect.create(app);
+      redirect.dispatch(actions.Redirect.Action.REMOTE, normalizedLink.href);
     }
   }
 
--- lib/shopify_app/engine.rb
+++ lib/shopify_app/engine.rb
@@ -16,6 +16,7 @@ module ShopifyApp
 
     initializer "shopify_app.assets.precompile" do |app|
       app.config.assets.precompile += %w[
+        shopify_app/app_bridge_redirect_1.30.0.js
         shopify_app/redirect.js
         shopify_app/post_redirect.js
         shopify_app/top_level.js
