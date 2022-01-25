090825efa3d00d29561300f2bee6d9c330a5d2ae
Hanna Chen <hanna.chen@shopify.com>
2022-01-25 10:33:36 -0500

    Fix typos

--- app/assets/javascripts/shopify_app/redirect.js
+++ app/assets/javascripts/shopify_app/redirect.js
@@ -1,6 +1,6 @@
 //= require ./app_bridge_redirect.js
 
-(function() {
+(function () {
   function redirect() {
     var redirectTargetElement = document.getElementById("redirection-target");
 
@@ -8,15 +8,16 @@
       return;
     }
 
-    var targetInfo = JSON.parse(redirectTargetElement.dataset.target)
+    var targetInfo = JSON.parse(redirectTargetElement.dataset.target);
 
     if (window.top == window.self) {
       // If the current window is the 'parent', change the URL by setting location.href
       window.top.location.href = targetInfo.url;
     } else {
-      // If the current window is the 'child' or embedded, change the parent's URL with App Bridge redirect
-      // This case can happen when an app updates its access scopes, or when the shop thinks the app is installed, but
-      // the app does not have shop entry
+      // If the current window is the 'child' or embedded, change the parent's URL with
+      // App Bridge redirect. This case can happen when an app updates its access scopes,
+      // or the unlikely scenario where the shop thinks the app is installed, but the
+      // app does not have an record for the shop.
       window.appBridgeRedirect(targetInfo.url);
     }
   }
