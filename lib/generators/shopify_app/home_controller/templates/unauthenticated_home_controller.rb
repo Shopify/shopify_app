99a511d2561353d5f5f4d47ca1feaa4b0adfde81
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-08-05 15:39:21 -0400

    Add call to embedded_app_url in home controller

--- lib/generators/shopify_app/home_controller/templates/unauthenticated_home_controller.rb
+++ lib/generators/shopify_app/home_controller/templates/unauthenticated_home_controller.rb
@@ -6,7 +6,22 @@ class HomeController < ApplicationController
   include ShopifyApp::ShopAccessScopesVerification
 
   def index
-    @shop_origin = current_shopify_domain
-    @host = params[:host]
+    if ShopifyAPI::Context.embedded? && !params[:embedded].present? || params[:embedded] != "1"
+      # TODO: replace this param with ShopifyAPI::Auth.embedded_app_url or whatever the new
+      # method name will be, once https://github.com/Shopify/shopify-api-ruby/pull/1002 is merged
+      redirect_to(embedded_app_url(params[:host]), allow_other_host: true)
+    else
+      @shop_origin = current_shopify_domain
+      @host = params[:host]
+    end
+  end
+
+  private
+
+  # TODO: remove this once https://github.com/Shopify/shopify-api-ruby/pull/1002
+  # is merged and released
+  def embedded_app_url(host)
+    decoded_host = Base64.decode64(host)
+    "https://#{decoded_host}/apps/#{ShopifyAPI::Context.api_key}"
   end
 end
