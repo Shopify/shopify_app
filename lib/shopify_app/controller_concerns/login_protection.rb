c777e39a1eb8ae001783098adae887e6bfbc287d
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-08-11 16:36:08 -0400

    Remove duplicate methods from LoginProtection
    Forgot to remove methods from LoginProtection that were moved to
    SanitizedParams during refactoring.

--- lib/shopify_app/controller_concerns/login_protection.rb
+++ lib/shopify_app/controller_concerns/login_protection.rb
@@ -196,35 +196,6 @@ module ShopifyApp
       raise ShopifyDomainNotFound
     end
 
-    def sanitized_shop_name
-      @sanitized_shop_name ||= sanitize_shop_param(params)
-    end
-
-    def referer_sanitized_shop_name
-      return unless request.referer.present?
-
-      @referer_sanitized_shop_name ||= begin
-        referer_uri = URI(request.referer)
-        query_params = Rack::Utils.parse_query(referer_uri.query)
-
-        sanitize_shop_param(query_params.with_indifferent_access)
-      end
-    end
-
-    def sanitize_shop_param(params)
-      return unless params[:shop].present?
-
-      ShopifyApp::Utils.sanitize_shop_domain(params[:shop])
-    end
-
-    def sanitized_params
-      request.query_parameters.clone.tap do |query_params|
-        if params[:shop].is_a?(String)
-          query_params[:shop] = sanitize_shop_param(params)
-        end
-      end
-    end
-
     def return_address
       return_address_with_params(shop: current_shopify_domain, host: host)
     rescue ShopifyDomainNotFound, ShopifyHostNotFound
