f56761461eaa90d4b336b1bf364669e931970442
Andy Waite <13400+andyw8@users.noreply.github.com>
2022-09-08 09:38:27 -0400

    Remove excess trailing `;` for CSP (#1500)
    This results in an extra `;`, e.g.:
    
    `frame-ancestors https://*.myshopify.com https://admin.shopify.com;;`

--- lib/shopify_app/controller_concerns/frame_ancestors.rb
+++ lib/shopify_app/controller_concerns/frame_ancestors.rb
@@ -8,7 +8,7 @@ module ShopifyApp
       content_security_policy do |policy|
         policy.frame_ancestors(-> do
           domain_host = current_shopify_domain || "*.myshopify.com"
-          "https://#{domain_host} https://admin.shopify.com;"
+          "https://#{domain_host} https://admin.shopify.com"
         end)
       end
     end
