67f989dfb6d8b80ac43db666ce415e870b8ef443
Nelson Wittwer <nelson.wittwer@shopify.com>
2022-09-14 15:39:15 -0400

    try without current_shopify_domain

--- lib/shopify_app/controller_concerns/frame_ancestors.rb
+++ lib/shopify_app/controller_concerns/frame_ancestors.rb
@@ -7,7 +7,7 @@ module ShopifyApp
     included do
       content_security_policy do |policy|
         policy.frame_ancestors(-> do
-          domain_host = current_shopify_domain || "*.#{::ShopifyApp::Configuration.myshopify_domain}"
+          domain_host = "*.#{::ShopifyApp::Configuration.myshopify_domain}"
           "https://#{domain_host} https://admin.shopify.com"
         end)
       end
