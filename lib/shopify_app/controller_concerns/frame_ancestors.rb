205feec7431fa5e1dca5447074ffa7981c01cfc9
Tatsuya Oiwa <tatsuya.oiwa@shopify.com>
2022-11-04 10:33:49 -0400

    Prevent errors caused by CSP frame_ancestors

--- lib/shopify_app/controller_concerns/frame_ancestors.rb
+++ lib/shopify_app/controller_concerns/frame_ancestors.rb
@@ -7,7 +7,8 @@ module ShopifyApp
     included do
       content_security_policy do |policy|
         policy.frame_ancestors(-> do
-          domain_host = current_shopify_domain || "*.#{::ShopifyApp.configuration.myshopify_domain}"
+          shop_domain = ShopifyApp::Utils.sanitize_shop_domain(params[:shop]) if params[:shop].present?
+          domain_host = shop_domain || "*.#{::ShopifyApp.configuration.myshopify_domain}"
           "#{ShopifyAPI::Context.host_scheme}://#{domain_host} https://admin.shopify.com"
         end)
       end
