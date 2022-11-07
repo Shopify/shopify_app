ed084e715ad5d50a8222644aed8bc7bb07a79837
Nelson Wittwer <nelson.wittwer@shopify.com>
2022-11-07 13:49:46 -0500

    move load_current_session to login_protection

--- lib/shopify_app/controller_concerns/login_protection.rb
+++ lib/shopify_app/controller_concerns/login_protection.rb
@@ -39,7 +39,7 @@ module ShopifyApp
     def current_shopify_session
       @current_shopify_session ||= begin
         cookie_name = ShopifyAPI::Auth::Oauth::SessionCookie::SESSION_COOKIE_NAME
-        ShopifyAPI::Utils::SessionUtils.load_current_session(
+        load_current_session(
           auth_header: request.headers["HTTP_AUTHORIZATION"],
           cookies: { cookie_name => cookies.encrypted[cookie_name] },
           is_online: user_session_expected?,
@@ -237,6 +237,71 @@ module ShopifyApp
       return false if ShopifyApp.configuration.shop_access_scopes_strategy.update_access_scopes?(shop_session.shop)
 
       !ShopifyApp.configuration.user_session_repository.blank? && ShopifyApp::SessionRepository.user_storage.present?
+    end
+
+    private
+
+    def load_current_session(auth_header: nil, cookies: nil, is_online: false)
+      return load_private_session if ShopifyAPI::Context.private?
+
+      session_id = current_session_id(auth_header, cookies, is_online)
+      return nil unless session_id
+
+      ShopifyApp::SessionRepository.load_session(session_id)
+    end
+
+    def load_private_session
+      unless ShopifyAPI::Context.private_shop
+        raise Errors::SessionNotFoundError, "Could not load private shop, Context.private_shop is nil."
+      end
+
+      Auth::Session.new(
+        shop: ShopifyAPI::Context.private_shop,
+        access_token: ShopifyAPI::Context.api_secret_key,
+        scope: ShopifyAPI::Context.scope.to_a,
+      )
+    end
+
+    def current_session_id(auth_header, cookies, online)
+      if ShopifyApp.configuration.embedded_app?
+        if auth_header
+          matches = auth_header.match(/^Bearer (.+)$/)
+          raise Errors::MissingJwtTokenError,
+            "Missing Bearer token in authorization header" unless matches
+
+          jwt_payload = ShopifyAPI::Auth::JwtPayload.new(matches[1])
+          shop = jwt_payload.shop
+
+          if online
+            jwt_session_id(shop, jwt_payload.sub)
+          else
+            offline_session_id(shop)
+          end
+        else
+          # falling back to session cookie
+          raise ShopifyAPI::Errors::CookieNotFoundError, "JWT token or Session cookie not found for app" unless
+          cookies && cookies[ShopifyAPI::Auth::Oauth::SessionCookie::SESSION_COOKIE_NAME]
+
+          cookie_session_id(cookies)
+        end
+      else
+        raise Errors::CookieNotFoundError, "Session cookie not found for app" unless
+        cookies && cookies[ShopifyAPI::Auth::Oauth::SessionCookie::SESSION_COOKIE_NAME]
+
+        cookie_session_id(cookies)
+      end
+    end
+
+    def jwt_session_id(shop, user_id)
+      "#{shop}_#{user_id}"
+    end
+
+    def offline_session_id(shop)
+      "offline_#{shop}"
+    end
+
+    def cookie_session_id(cookies)
+      cookies[ShopifyAPI::Auth::Oauth::SessionCookie::SESSION_COOKIE_NAME] # FIXME - should we move to library as well?
     end
   end
 end
