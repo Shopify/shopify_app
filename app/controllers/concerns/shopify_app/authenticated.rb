728fbb72997d9ebe34877ad9cde1683d5e587070
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-11-14 10:50:27 -0500

    fixing include issue

--- app/controllers/concerns/shopify_app/authenticated.rb
+++ app/controllers/concerns/shopify_app/authenticated.rb
@@ -2,11 +2,12 @@
 
 module ShopifyApp
   module Authenticated
-    extend ShopifyApp::EnsureHasSession
     extend ActiveSupport::Concern
 
     included do
       ShopifyApp::Logger.deprecated("Authenticated has been renamed to EnsureHasSession")
     end
+
+    include ShopifyApp::EnsureHasSession
   end
 end
--- app/controllers/concerns/shopify_app/require_known_shop.rb
+++ app/controllers/concerns/shopify_app/require_known_shop.rb
@@ -3,47 +3,11 @@
 module ShopifyApp
   module RequireKnownShop
     extend ActiveSupport::Concern
-    include ShopifyApp::RedirectForEmbedded
 
     included do
-      # ShopifyApp::Logger.deprecated("RequireKnownShop has been renamed to EnsureInstalled")
-      before_action :check_shop_domain
-      before_action :check_shop_known
+      ShopifyApp::Logger.deprecated("RequireKnownShop has been renamed to EnsureInstalled. Please use EnsureInstalled controller concern for the same behavior")
     end
-
-    def current_shopify_domain
-      return if params[:shop].blank?
-
-      @shopify_domain ||= ShopifyApp::Utils.sanitize_shop_domain(params[:shop])
-    end
-
-    private
-
-    def check_shop_domain
-      redirect_to(ShopifyApp.configuration.login_url) unless current_shopify_domain
-    end
-
-    def check_shop_known
-      @shop = SessionRepository.retrieve_shop_session_by_shopify_domain(current_shopify_domain)
-      unless @shop
-        if embedded_param?
-          redirect_for_embedded
-        else
-          redirect_to(shop_login)
-        end
-      end
-    end
-
-    def shop_login
-      url = URI(ShopifyApp.configuration.login_url)
-
-      url.query = URI.encode_www_form(
-        shop: params[:shop],
-        host: params[:host],
-        return_to: request.fullpath,
-      )
-
-      url.to_s
-    end
+    
+    include ShopifyApp::EnsureInstalled
   end
 end
