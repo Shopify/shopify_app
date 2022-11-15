2f844ef29a0d65c20430cf08d7d8af53d82da0e1
Andy Waite <13400+andyw8@users.noreply.github.com>
2022-11-15 12:12:13 -0500

    Avoid overly-general rescue in callback controller (#1530)
    * Avoid overly-general rescue in callback controller
    
    * Deprecation error
    
    * Add tests
    
    * Update CHANGELOG
    
    Co-authored-by: Andy Waite <andyw8@users.noreply.github.com>

--- app/controllers/shopify_app/callback_controller.rb
+++ app/controllers/shopify_app/callback_controller.rb
@@ -17,7 +17,14 @@ module ShopifyApp
           },
           auth_query: ShopifyAPI::Auth::Oauth::AuthQuery.new(**filtered_params),
         )
-      rescue
+      rescue => e
+        unless e.class.module_parent == ShopifyAPI::Errors
+          ActiveSupport::Deprecation.warn(<<~EOS)
+            An error of type #{e.class} was rescued. This is not part of `ShopifyAPI::Errors`, which could indicate a
+            bug in your app, or a bug in the shopify_app gem. Future versions of the gem may re-raise this error rather
+            than rescuing it.
+          EOS
+        end
         return respond_with_error
       end
 
