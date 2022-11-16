3cde1ee3e5b2f57540a51cf277d55e5f58957d4f
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-11-16 13:09:16 -0500

    add depreaction tests for ci

--- app/controllers/concerns/shopify_app/authenticated.rb
+++ app/controllers/concerns/shopify_app/authenticated.rb
@@ -5,8 +5,8 @@ module ShopifyApp
     extend ActiveSupport::Concern
 
     included do
-      ShopifyApp::Logger.deprecated("RequireKnownShop has been replaced by to EnsureInstalled."\
-        " Please use EnsureInstalled controller concern for the same behavior", "22.0.0")
+      ShopifyApp::Logger.deprecated("Authenticated has been replaced by to EnsureHasSession."\
+        " Please use EnsureHasSession controller concern for the same behavior", "22.0.0")
     end
 
     include ShopifyApp::EnsureHasSession
