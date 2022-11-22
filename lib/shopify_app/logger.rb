540394b9210a78b5db9f559704210411bfd195c7
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-11-22 16:18:02 -0500

    update context to ShopifyApp

--- lib/shopify_app/logger.rb
+++ lib/shopify_app/logger.rb
@@ -5,9 +5,14 @@ module ShopifyApp
     def self.deprecated(message, version)
       return unless enabled_for_log_level?(:warn)
 
-      raise StandardError unless ShopifyApp::VERSION < version
+      raise ShopifyAPI::Errors::FeatureDeprecatedError unless ShopifyApp::VERSION < version
 
       ActiveSupport::Deprecation.warn("[#{version}] #{context(:warn)} #{message}")
+    end
+
+    def self.context(log_level)
+      current_shop = ShopifyAPI::Context.active_session&.shop || "Shop Not Found"
+      "[ ShopifyApp | #{log_level.to_s.upcase} | #{current_shop} ]"
     end
   end
 end
