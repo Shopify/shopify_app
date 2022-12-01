5b22cf8a53e5632b00fd6543088467e072fc1640
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-12-01 12:40:50 -0500

    rubocop

--- test/controllers/concerns/ensure_installed_test.rb
+++ test/controllers/concerns/ensure_installed_test.rb
@@ -63,7 +63,6 @@ class EnsureInstalledTest < ActionController::TestCase
 
   test "detects incompatible controller concerns" do
     parent_deprecation_setting = ActiveSupport::Deprecation.silenced
-    parent_context_log_level = ShopifyAPI::Context.log_level
     ActiveSupport::Deprecation.silenced = false
     ShopifyAPI::Context.stubs(:log_level).returns(:warn)
 
