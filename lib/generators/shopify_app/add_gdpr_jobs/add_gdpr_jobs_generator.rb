1a293c3175f6c935b6a5a6af013d0d35cdc437fb
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-12-01 15:05:47 -0500

    fix gdpr names

--- lib/generators/shopify_app/add_gdpr_jobs/add_gdpr_jobs_generator.rb
+++ lib/generators/shopify_app/add_gdpr_jobs/add_gdpr_jobs_generator.rb
@@ -8,7 +8,7 @@ module ShopifyApp
       source_root File.expand_path("../templates", __FILE__)
 
       def add_customer_data_request_job
-        template("customer_data_request_job.rb", "app/jobs/customer_data_request_job.rb")
+        template("customers_data_request_job.rb", "app/jobs/customers_data_request_job.rb")
       end
 
       def add_shop_redact_job
@@ -16,7 +16,7 @@ module ShopifyApp
       end
 
       def add_customer_redact_job
-        template("customer_redact_job.rb", "app/jobs/customer_redact_job.rb")
+        template("customers_redact_job.rb", "app/jobs/customers_redact_job.rb")
       end
     end
   end
