23df405bd8abcf634d7917e6f933e1c8ccd2cf1a
Bill Klenotiz <klenotiw@users.noreply.github.com>
2022-12-02 09:43:55 -0500

    small changes

--- app/controllers/concerns/shopify_app/require_known_shop.rb
+++ app/controllers/concerns/shopify_app/require_known_shop.rb
@@ -5,14 +5,9 @@ module ShopifyApp
     extend ActiveSupport::Concern
     include ShopifyApp::EnsureInstalled
 
-    def self.extended(mod)
-      ShopifyApp::Logger.deprecated("RequireKnownShop has been replaced by to EnsureInstalled."\
-        " Please use EnsureInstalled controller concern for the same behavior", "22.0.0")
-    end
-
-    def self.included(mod)
-      ShopifyApp::Logger.deprecated("RequireKnownShop has been replaced by to EnsureInstalled."\
-        " Please use EnsureInstalled controller concern for the same behavior", "22.0.0")
+    included do
+      ShopifyApp::Logger.deprecated("RequireKnownShop has been replaced by EnsureInstalled."\
+        " Please use the EnsureInstalled controller concern for the same behavior", "22.0.0")
     end
   end
 end
