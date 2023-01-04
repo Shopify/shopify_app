d6caf9b00b37c4490c85541bff34fdcd2c68c480
Nelson <nelsonwittwer@users.noreply.github.com>
2023-01-04 08:59:16 -0500

    Allow a more complex customized `myshopify_domain` (#1620)
    * allow for complex myshopify_domains
    
    * better adaptation to existing test cases
    
    * Changelog
    
    * assume that tld domain checks are sufficent
    
    * better scheme check
    
    * extract util private methods;  better edge cases
    
    * remove misdirecting check
    
    * add addressable to requirements
    
    Co-authored-by: Bill Klenotiz <klenotiw@users.noreply.github.com>

--- lib/shopify_app/utils.rb
+++ lib/shopify_app/utils.rb
@@ -2,38 +2,69 @@
 
 module ShopifyApp
   module Utils
-    TRUSTED_SHOPIFY_DOMAINS = [
-      "shopify.com",
-      "myshopify.io",
-      "myshopify.com",
-    ].freeze
+    class << self
+      TRUSTED_SHOPIFY_DOMAINS = [
+        "shopify.com",
+        "myshopify.io",
+        "myshopify.com",
+        "spin.dev",
+      ].freeze
 
-    def self.sanitize_shop_domain(shop_domain)
-      myshopify_domain = ShopifyApp.configuration.myshopify_domain
-      name = shop_domain.to_s.downcase.strip
-      name += ".#{myshopify_domain}" if !name.include?(myshopify_domain.to_s) && !name.include?(".")
-      name.sub!(%r|https?://|, "")
-      trusted_domains = TRUSTED_SHOPIFY_DOMAINS.dup.push(myshopify_domain)
+      def sanitize_shop_domain(shop_domain)
+        uri = uri_from_shop_domain(shop_domain)
+        return nil if uri.nil?
 
-      u = URI("http://#{name}")
-      regex = /^[a-z0-9][a-z0-9\-]*[a-z0-9]\.(#{trusted_domains.join("|")})$/
-      u.host if u.host&.match(regex)
-    rescue URI::InvalidURIError
-      nil
-    end
+        trusted_domains.each do |trusted_domain|
+          no_shop_name_in_subdomain = uri.host == trusted_domain
+          from_trusted_domain = trusted_domain == uri.domain
 
-    def self.shop_login_url(shop:, host:, return_to:)
-      return ShopifyApp.configuration.login_url unless shop
+          return nil if no_shop_name_in_subdomain || uri.host&.empty?
+          return uri.host if from_trusted_domain
+        end
 
-      url = URI(ShopifyApp.configuration.login_url)
+        nil
+      end
 
-      url.query = URI.encode_www_form(
-        shop: shop,
-        host: host,
-        return_to: return_to,
-      )
+      def shop_login_url(shop:, host:, return_to:)
+        return ShopifyApp.configuration.login_url unless shop
 
-      url.to_s
+        url = URI(ShopifyApp.configuration.login_url)
+
+        url.query = URI.encode_www_form(
+          shop: shop,
+          host: host,
+          return_to: return_to,
+        )
+
+        url.to_s
+      end
+
+      private
+
+      def myshopify_domain
+        ShopifyApp.configuration.myshopify_domain
+      end
+
+      def trusted_domains
+        trusted_domains = TRUSTED_SHOPIFY_DOMAINS.dup
+        trusted_domains.append(myshopify_domain).uniq! if myshopify_domain
+        trusted_domains
+      end
+
+      def uri_from_shop_domain(shop_domain)
+        name = shop_domain.to_s.downcase.strip
+        name += ".#{myshopify_domain}" if !name.include?(myshopify_domain.to_s) && !name.include?(".")
+        uri = Addressable::URI.parse(name)
+
+        if uri.scheme.nil?
+          name = "https://" + name
+          uri = Addressable::URI.parse(name)
+        end
+
+        uri
+      rescue Addressable::URI::InvalidURIError
+        nil
+      end
     end
   end
 end
