e146d9aa3832da3fe558b02b97cfb07c86d3eeb2
Jed Barlow <jed@jedbarlow.ca>
2023-04-14 08:03:02 -0700

    switch to latest rspec stub syntax in rspec session helper

--- lib/shopify_app/test_helpers/shopify_rspec_session_helper.rb
+++ lib/shopify_app/test_helpers/shopify_rspec_session_helper.rb
@@ -5,8 +5,8 @@ module ShopifyApp
     module ShopifyRSpecSessionHelper
       def setup_shopify_session(session_id:, shop_domain:)
         ShopifyAPI::Auth::Session.new(id: session_id, shop: shop_domain).tap do |session|
-          ShopifyApp::SessionRepository.stub(:load_session) { session }
-          ShopifyAPI::Utils::SessionUtils.stub(:current_session_id) { session.id }
+          allow(ShopifyApp::SessionRepository).to receive(:load_session) { session }
+          allow(ShopifyAPI::Utils::SessionUtils).to receive(:current_session_id) { session.id }
           ShopifyAPI::Context.activate_session(session)
         end
       end
