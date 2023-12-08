39ec9beed49262ac86c1595d59634dbd1405bb54
Remy Wang <remy.wang727@gmail.com>
2023-12-08 18:26:42 +0100

    Fix: lint

--- lib/generators/shopify_app/add_webhook/add_webhook_generator.rb
+++ lib/generators/shopify_app/add_webhook/add_webhook_generator.rb
@@ -41,10 +41,10 @@ module ShopifyApp
       def add_webhook_job
         namespace = ShopifyApp.configuration.webhook_jobs_namespace
         @job_file_name = if namespace.present?
-                           "#{namespace}/#{job_file_name}_job"
-                         else
-                           "#{job_file_name}_job"
-                         end
+          "#{namespace}/#{job_file_name}_job"
+        else
+          "#{job_file_name}_job"
+        end
         @job_class_name = @job_file_name.classify
         template("webhook_job.rb", "app/jobs/#{@job_file_name}.rb")
       end
