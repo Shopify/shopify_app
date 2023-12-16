b26e07d41cf4cb9d713374058e51d32a27c19a5a
Stefan Vermaas <stefanvermaas@pm.me>
2023-12-16 05:33:37 +0100

    Enable dynamic definition of the embedded app layout
    When running a Shopify app in embedded mode, every controller that
    requires an authenticated session will use the embedded app layout
    by default.
    
    As a default, this is great. It helps the developer to make less
    choices when they start to develop their new Shopify app. However,
    sometimes one wants to run an embedded app in non-embedded mode.
    
    Some examples of running a normally embedded app in non-embedded mode:
    - When the remote environment is a CI;
    - When the remote environment is a preview/PR app;
    - When the developer wants to run the app as a non-embedded mode for testing.
    
    The rather static implementation of the embedded app layout causes
    duplication and maintainability issues when running in both modes.
    
    This commit introduces a more flexible system whereby the deverloper
    has more control over what layout is being rendered during the process
    and it allows the developer to ignore the embedded app layout.
    
    Instead, the developer can implement the embedded app layout (as
    provided by the gem) once in the application.html.erb, and set the
    
    Now, when running the app both in embedded and non-embedded mode,
    the developer only has to maintain a single layout file for all
    controllers in its app.

---
+++ test/controllers/concerns/embedded_app_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "action_view/testing/resolvers"
+
+class EmbeddedAppTest < ActionController::TestCase
+  class BaseTestController < ActionController::Base
+    abstract!
+
+    self.view_paths = [
+      ActionView::FixtureResolver.new(
+        "layouts/application.erb"                             => "Application Layout <%= yield %>",
+        "layouts/embedded_app.erb"                            => "Embedded App Layout <%= yield %>",
+        "embedded_app_test/embedded_app_test/index.html.erb"  => "OK",
+      ),
+    ]
+  end
+
+  class ApplicationTestController < BaseTestController
+    layout "application"
+  end
+
+  class EmbeddedAppTestController < ApplicationTestController
+    include ShopifyApp::EmbeddedApp
+
+    def index; end
+  end
+
+  tests EmbeddedAppTestController
+
+  setup do
+    Rails.application.routes.draw do
+      get "/embedded_app", to: "embedded_app_test/embedded_app_test#index"
+    end
+  end
+
+  test "uses the embedded app layout when running in embedded mode" do
+    ShopifyApp.configuration.embedded_app = true
+
+    get :index
+    assert_template layout: "embedded_app"
+  end
+
+  test "uses the default layout when running in non-embedded mode" do
+    ShopifyApp.configuration.embedded_app = false
+
+    get :index
+    assert_template layout: "application"
+  end
+end
