8bbc45edb6433464f04975863b1a36fb1436894a
Ugurcan Kaya <uurcan@pasilobus.com>
2024-04-08 12:23:17 -0700

    Update token_exchange.rb

--- lib/shopify_app/controller_concerns/token_exchange.rb
+++ lib/shopify_app/controller_concerns/token_exchange.rb
@@ -5,29 +5,23 @@ module ShopifyApp
     extend ActiveSupport::Concern
 
     def activate_shopify_session
-      if current_shopify_session.blank?
-        retrieve_session_from_token_exchange
+      begin
+        if current_shopify_session.blank?
+          retrieve_session_from_token_exchange
+        end
+
+        if ShopifyApp.configuration.check_session_expiry_date && current_shopify_session.expired?
+          @current_shopify_session = nil
+          retrieve_session_from_token_exchange
+        end
+      rescue ShopifyAPI::Errors::CookieNotFoundError
+        return respond_to_invalid_session_token
       end
 
-      if ShopifyApp.configuration.check_session_expiry_date && current_shopify_session.expired?
-        retrieve_session_from_token_exchange
-      end
-
-      attempts = 0
       begin
         ShopifyApp::Logger.debug("Activating Shopify session")
         ShopifyAPI::Context.activate_session(current_shopify_session)
         yield
-      rescue ShopifyAPI::Errors::HttpResponseError => error
-        if error.code == 401 && attempts.zero?
-          ShopifyApp::Logger.debug("Encountered 401 error, exchanging token and retrying with new access token")
-          attempts += 1
-          retrieve_session_from_token_exchange
-          retry
-        else
-          ShopifyApp::Logger.debug("Encountered error: #{error.code} - #{error.message}, re-raising")
-          raise
-        end
       ensure
         ShopifyApp::Logger.debug("Deactivating session")
         ShopifyAPI::Context.deactivate_session
@@ -56,7 +50,6 @@ module ShopifyApp
     private
 
     def retrieve_session_from_token_exchange
-      @current_shopify_session = nil
       # TODO: Right now JWT Middleware only updates env['jwt.shopify_domain'] from request headers tokens,
       # which won't work for new installs.
       # we need to update the middleware to also update the env['jwt.shopify_domain'] from the query params
@@ -128,21 +121,21 @@ module ShopifyApp
     def respond_to_invalid_session_token
       # TODO: Implement this method to handle invalid session tokens
 
-      # if request.xhr?
-      # response.set_header("X-Shopify-Retry-Invalid-Session-Request", 1)
-      # unauthorized_response = { message: :unauthorized }
-      # render(json: { errors: [unauthorized_response] }, status: :unauthorized)
-      # else
-      # patch_session_token_url = "#{ShopifyAPI::Context.host}/patch_session_token"
-      # patch_session_token_params = request.query_parameters.except(:id_token)
+      if request.xhr?
+        response.set_header("X-Shopify-Retry-Invalid-Session-Request", 1)
+        unauthorized_response = { message: :unauthorized }
+        render(json: { errors: [unauthorized_response] }, status: :unauthorized)
+      else
+        patch_session_token_url = "#{ShopifyApp.configuration.root_url}/patch_session_token"
+        patch_session_token_params = request.query_parameters.except(:id_token)
 
-      # bounce_url = "#{ShopifyAPI::Context.host}#{request.path}?#{patch_session_token_params.to_query}"
+        bounce_url = "#{request.path}?#{patch_session_token_params.to_query}"
 
-      # # App Bridge will trigger a fetch to the URL in shopify-reload, with a new session token in headers
-      # patch_session_token_params["shopify-reload"] = bounce_url
+        # App Bridge will trigger a fetch to the URL in shopify-reload, with a new session token in headers
+        patch_session_token_params["shopify-reload"] = bounce_url
 
-      # redirect_to("#{patch_session_token_url}?#{patch_session_token_params.to_query}", allow_other_host: true)
-      # end
+        redirect_to("#{patch_session_token_url}?#{patch_session_token_params.to_query}", allow_other_host: true)
+      end
     end
 
     def online_token_configured?
