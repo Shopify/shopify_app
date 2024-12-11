726e4f4744aba63ae97d37c8cd8c643d239dcd9b
Matt Oakley <matt@supercycle.com>
2024-12-11 14:33:33 +0000

    Reimplement frame_ancestors proc to fix tests
    @sle-c has pointed out that the tests rely on the proc
    and suggests reimplemeting the proc and returning an array.
    
    This patch implements the recommendation and achieves the
    same result.

--- lib/shopify_app/controller_concerns/frame_ancestors.rb
+++ lib/shopify_app/controller_concerns/frame_ancestors.rb
@@ -6,8 +6,13 @@ module ShopifyApp
 
     included do
       content_security_policy do |policy|
-        domain_host = current_shopify_domain || "*.#{::ShopifyApp.configuration.myshopify_domain}"
-        policy.frame_ancestors "#{ShopifyAPI::Context.host_scheme}://#{domain_host}", "https://admin.#{::ShopifyApp.configuration.unified_admin_domain}"
+        policy.frame_ancestors(-> do
+          domain_host = current_shopify_domain || "*.#{::ShopifyApp.configuration.myshopify_domain}"
+          [
+            "#{ShopifyAPI::Context.host_scheme}://#{domain_host}",
+            "https://admin.#{::ShopifyApp.configuration.unified_admin_domain}",
+          ]
+        end)
       end
     end
   end
